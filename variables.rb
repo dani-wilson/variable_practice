ff743df771e70f25ee02f6ff3775833027b6ecd1
Dani Wilson <danirae.powell@gmail.com>
2023-03-25 12:09:20 -0600

    Add quotations to puts

--- variables.rb
+++ variables.rb
@@ -1,23 +1,23 @@
-name = "danirae";
-username = "dayrae12";
-caption = "Hey there!";
+name = "danirae"
+username = "dayrae12"
+caption = "Hey there!"
 
-age = 28;
-dob = 07/13/1994;
+age = 28
+dob = 07/13/1994
 
-account_balance = 1276.91;
-distance_traveled = 78.5;
+account_balance = 1276.91
+distance_traveled = 78.5
 
-balance_paid = True;
-assignment_completed = false;
+balance_paid = True
+assignment_completed = false
 
-x = name;
+x = name
 
-puts "I dont know if I did that correctly";
-puts "But I hope that I did";
-puts "I guess we will see soon enough!";
+puts "I dont know if I did that correctly"
+puts "But I hope that I did"
+puts "I guess we will see soon enough!"
 
-puts name;
-puts username;
-puts caption;
+puts name
+puts username
+puts caption
 
